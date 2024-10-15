c411fd985f602f3151b8e21ab67ee1e46595ef51
Arumiir <127603214+Arumiir@users.noreply.github.com>
2024-10-15 17:06:27 -0300

    Atualizar o service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,21 +1,43 @@
+const CACHE_NAME = 'pwa-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/horas/index.html',
+  '/cargas/index.html',
+  '/style.css',
+  '/icons/icon-192x192.png',
+  '/icons/icon-512x512.png'
+];
+
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open('pwa-cache').then(cache => {
-      return cache.addAll([
-        '/index.html',
-        '/resultados.html',
-        '/detalhes.html',
-        '/styles.css',
-        '/manifest.json'
-      ]);
-    })
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
   );
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
+
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
     })
   );
 });
