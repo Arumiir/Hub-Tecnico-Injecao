c1b1924d444c6b739ec76ca212982d5771a41ce2
Arumiir <127603214+Arumiir@users.noreply.github.com>
2024-10-15 20:12:53 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,22 +1,48 @@
+// Service worker otimizado e atualizado
+
 self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open('app-cache').then(cache => {
-      return cache.addAll([
-        '/',
-        '/index.html',
-        '/style.css',
-        '/service-worker.js',
-        '/icons/icon-192x192.png',
-        '/icons/icon-512x512.png'
-      ]);
-    })
-  );
+    event.waitUntil(
+        caches.open('app-cache-v2').then(cache => {
+            return cache.addAll([
+                '/',
+                '/index.html',
+                '/style.css',
+                '/service-worker.js',
+                '/icons/icon-192x192.png',
+                '/icons/icon-512x512.png',
+                // Adicione outros arquivos necessários aqui
+            ]);
+        })
+    );
+});
+
+self.addEventListener('activate', event => {
+    const cacheWhitelist = ['app-cache-v2']; // Lista de caches a manter
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cacheName => {
+                    if (!cacheWhitelist.includes(cacheName)) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
 });
 
 self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
-  );
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            if (response) {
+                return response;
+            }
+            return fetch(event.request).then(fetchResponse => {
+                return caches.open('app-cache-v2').then(cache => {
+                    cache.put(event.request, fetchResponse.clone());
+                    return fetchResponse;
+                });
+            });
+        })
+    );
 });
