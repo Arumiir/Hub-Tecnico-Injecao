590f882230b9619dae070d27829ef1602d25e148
Arumiir <127603214+Arumiir@users.noreply.github.com>
2026-01-06 13:15:35 -0300

    Atualizar o service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,53 +1,29 @@
-// Service worker otimizado para funcionalidade offline completa
+const CACHE_NAME = 'fabrica-app-v1';
+const ASSETS_TO_CACHE = [
+  './',
+  './index.html',
+  './regulagens.html',
+  './calculadoras.html',
+  './admin-login.html',
+  './admin-panel.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+  // Adicione mais arquivos conforme criar (ex: calculadoras específicas)
+];
 
 self.addEventListener('install', event => {
-    event.waitUntil(
-        caches.open('app-cache-v2').then(cache => {
-            return cache.addAll([
-                '/',
-                '/index.html',
-                '/style.css',
-                '/manifest.json',
-                '/service-worker.js',
-                '/icons/icon-192x192.png',
-                '/icons/icon-512x512.png',
-                '/horas/index.html', // Incluindo páginas adicionais
-                '/cargas/index.html', // Incluindo páginas adicionais
-                // Adicione outros arquivos e pastas necessários aqui
-            ]);
-        })
-    );
-});
-
-self.addEventListener('activate', event => {
-    const cacheWhitelist = ['app-cache-v2'];
-    event.waitUntil(
-        caches.keys().then(cacheNames => {
-            return Promise.all(
-                cacheNames.map(cacheName => {
-                    if (!cacheWhitelist.includes(cacheName)) {
-                        return caches.delete(cacheName);
-                    }
-                })
-            );
-        })
-    );
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
 });
 
 self.addEventListener('fetch', event => {
-    event.respondWith(
-        caches.match(event.request).then(response => {
-            if (response) {
-                return response;
-            }
-            return fetch(event.request).then(fetchResponse => {
-                return caches.open('app-cache-v2').then(cache => {
-                    if (event.request.url.startsWith(self.location.origin)) {
-                        cache.put(event.request, fetchResponse.clone());
-                    }
-                    return fetchResponse;
-                });
-            });
-        })
-    );
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
 });
